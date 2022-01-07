1e0c5243817157a15a4886e037ba04f3894773f2
Kushagra Kumar <72915833+k28611-nits@users.noreply.github.com>
2022-01-07 12:46:49 +0530

    Time: 3 ms (79.91%), Space: 12.2 MB (34.63%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+    void nextPermutation(vector<int> &nums)
+{
+    int breakPoint = -1;
+    for (int i = nums.size() - 1; i > 0; i--)
+    {
+        if (nums[i] > nums[i - 1])
+        {
+            breakPoint = i - 1;
+            break;
+        }
     }
+    if (breakPoint < 0)
+    {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+    for (int i = nums.size() - 1; i >= 0; i--)
+    {
+        if (nums[i] > nums[breakPoint])
+        {
+            swap(nums[breakPoint], nums[i]);
+            reverse(nums.begin() + breakPoint + 1, nums.end());
+            break;
+        }
+    }
+}
 };
