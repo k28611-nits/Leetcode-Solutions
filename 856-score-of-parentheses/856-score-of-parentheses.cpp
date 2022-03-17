3ffee4dca0961f367334fa25a9dabda0bdbc941a
Kushagra Kumar <72915833+k28611-nits@users.noreply.github.com>
2022-03-17 09:01:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.12%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        
+        stack<int> st;
+        int ind = 0;
+        
+        for(auto i : s)
+        {
+            if(i == '(')
+            {
+                st.push(ind);
+                ind = 0;
+            }
+            else 
+            {
+                ind = st.top() + max(ind*2 ,1);
+                st.pop();
+            }
+                
+        }
+        return ind;
+        
+    }
+};
