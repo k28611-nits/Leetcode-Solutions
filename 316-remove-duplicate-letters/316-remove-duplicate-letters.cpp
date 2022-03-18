86589c9ea6038d2e4c3255ed15417bd31877de36
Kushagra Kumar <72915833+k28611-nits@users.noreply.github.com>
2022-03-18 22:54:14 +0530

    Time: 3 ms (73.49%), Space: 6.6 MB (66.29%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> count(26,0);
+        for(int i=0;i<s.length();i++){
+            count[s[i]-'a']++;
+        }
+        vector<int> visited(26,0);
+        
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            count[s[i]-'a']--;
+            if(!visited[s[i]-'a']){
+                while(ans.length()>0 && ans.back()>s[i] && count[ans.back()-'a']>0){
+                    visited[ans.back()-'a']=0;
+                    ans.pop_back();
+                }
+                ans+=s[i];
+                visited[s[i]-'a']=1;
+            }
+        }
+        return ans;
+    }
+};
