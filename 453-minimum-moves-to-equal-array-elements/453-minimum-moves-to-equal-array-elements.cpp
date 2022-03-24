c6908cf747e6c180f5b70dd2df4dc68b36aabbe7
Kushagra Kumar <72915833+k28611-nits@users.noreply.github.com>
2022-03-24 11:45:09 +0530

    Time: 81 ms (14.01%), Space: 28.3 MB (42.55%) - LeetHub

---
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int m=INT_MAX;
+			for(int n:nums) m = min(m,n);
+			int ans=0;
+			for(int n:nums) ans+=n-m;
+			return ans;
+    }
+};
