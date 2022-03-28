fda8e4d46799840eae8105e8978f3396978260b6
Kushagra Kumar <72915833+k28611-nits@users.noreply.github.com>
2022-03-28 18:29:41 +0530

    Time: 89 ms (59.19%), Space: 48.2 MB (99.32%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool canJump(vector<int>& A) {
+      int i = 0,n=A.size();
+    for (int reach = 0; i < n && i <= reach; ++i)
+        reach = max(i + A[i], reach);
+    return i == n;
+    }
+};
