c51e5d19ffa0aea6995ecbcb7344b0a2ef4fc6fa
Kushagra Kumar <72915833+k28611-nits@users.noreply.github.com>
2022-04-08 19:59:26 +0530

    Time: 4 ms (25.76%), Space: 6.2 MB (92.22%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int>result;
-        long val =1; // long? to avoid overflow(go through constraints given)
+        long val =1;
         for(int i =0; i<= rowIndex; i++){
           result.push_back(val);
           val = val * (rowIndex - i) / (i+1);
