3436e5ccefa8cf608fa13d1211ae8537af725d90
Kushagra Kumar <72915833+k28611-nits@users.noreply.github.com>
2022-04-11 23:33:53 +0530

    Time: 4 ms (51.36%), Space: 10.4 MB (55.71%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& p) {
+         return p[0][0] * (p[1][1] - p[2][1]) + p[1][0] * (p[2][1] - p[0][1]) + p[2][0] * (p[0][1] - p[1][1]) != 0;
+    }
+};
