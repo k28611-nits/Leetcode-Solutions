0c683a68d57be14b631706be7fe7dd18f8842880
Kushagra Kumar <72915833+k28611-nits@users.noreply.github.com>
2022-06-09 22:16:36 +0530

    Added solution - LeetHub

---
+++ Union-Find - GFG/union-find.cpp
@@ -0,0 +1,91 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    void union_( int a, int b, int par[], int rank[]) 
+    {
+        int para = find(a,par);
+        int parb=find(b,par);
+        if(isConnected(a,b,par,rank))return;
+        if(rank[para]>rank[parb]){
+            rank[para]+=rank[parb];
+            par[parb]=para;
+        }
+        else{
+            rank[parb]+=rank[para];
+            par[para]=parb;
+        }
+    }
+    
+    
+    int find(int a,int par[]){
+        int cur = par[a];
+        while(cur!=par[cur]){
+            par[cur] = par[par[cur]];
+            cur = par[cur];
+        }
+        return cur;
+    }
+    
+    //Function to check whether 2 nodes are connected or not.
+    bool isConnected(int x,int y, int par[], int rank1[])
+    {
+        return find(x,par)==find(y,par);
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    //taking number of testcases
+    int t;
+    cin>>t;
+    while(t--) {
+        
+        //taking number of elements
+        int n;
+        cin>>n;
+        
+        
+        int par[n+1];
+        int rank1[n+1];
+
+        //initializing the parent and
+        //the rank array
+        for(int i=1; i<=n; i++) {
+            par[i] = i;
+            rank1[i] = 1;
+    }
+    
+    //taking number of queries
+    Solution obj;
+    int q;
+    cin>>q;
+    while(q--) {    
+        int a,b;
+        char c;
+        cin>>c>>a>>b;
+        
+        //if query type is 'U'
+        //then calling union_ method
+        if(c=='U')
+        {
+            obj.union_(a,b, par, rank1);
+        }
+        else//else calling isConnected() method
+        cout<<obj.isConnected(a,b, par, rank1)<<endl;
+    }
+    }
+    return 0;
+}
+  // } Driver Code Ends
