c705987a2a9f154b4de846b0fe4a41d9c2b80464
Kushagra Kumar <72915833+k28611-nits@users.noreply.github.com>
2022-07-22 10:40:21 +0530

    Time: 3 ms (96.33%), Space: 10.3 MB (32.53%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -1,56 +1,14 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    
-  ListNode *arrayToList(int arr[], int n)
-{
-    ListNode *root = NULL;
-    for (int i = 0; i < n; i++)
-        insert(&root, arr[i]);
-   return root;
-}
-    void insert(ListNode** root, int item)
-{
-    ListNode* temp = new ListNode;
-    ListNode* ptr;
-    temp->val = item;
-    temp->next = NULL;
-  
-    if (*root == NULL)
-        *root = temp;
-    else {
-        ptr = *root;
-        while (ptr->next != NULL)
-            ptr = ptr->next;
-        ptr->next = temp;
-    }
-}
     ListNode* partition(ListNode* head, int x) {
-        int n=0,m=0;
-        ListNode* dummy=head;
-        int a[201],b[201];
-        while(dummy!=NULL)
-        {
-            if(dummy->val <x){
-                a[n]=dummy->val; n++;}
-            else
-            { b[m]=dummy->val;m++;}
-            dummy=dummy->next;
+        ListNode *fdum = new ListNode(0), *bdum = new ListNode(0),
+                 *front = fdum, *back = bdum, *curr = head;
+        while (curr) {
+            if (curr->val < x) front->next = curr, front = curr;
+            else back->next = curr, back = curr;
+            curr = curr->next;
         }
-        for(int i=n,j=0;i<n+m;++i,j++)
-            a[i]=b[j];
-        int size=n+m;
-        ListNode* root=arrayToList(a,size);
-        
-            return root;
+        front->next = bdum->next, back->next = nullptr;
+        return fdum->next;
     }
 };
